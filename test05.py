a4cb55eabc1007ff345d6cc768c882241c40256a
tienshake <tientrancntt@gmail.com>
2022-04-26 23:14:05 +0700

    test cast 5

---
+++ test05.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+
+driver.get("https://fullstack.edu.vn") #step 1
+driver.implicitly_wait(6)
+#step 2 bam vao dang nhap
+driver.find_element(By.CSS_SELECTOR, '#root > div.NavBar_wrapper__4m3K5 > div.NavBar_actions__nSNzo > a').click()
+#step 3 Bấm vào button 'Sử dụng email/số điện thoại'
+driver.find_element(By.CSS_SELECTOR, '#root > div > div.Login_container__l\+22X > div.Login_content__Clzqv > div.Login_body__t6Xgp > div > div:nth-child(1)').click()
+#Step 4: Nhập số điện thoại vào ô nhập số điện thoại
+driver.find_element(By.CSS_SELECTOR, '#root > div > div.Login_container__l\+22X > div.Login_content__Clzqv > div.Login_body__t6Xgp > div > div:nth-child(1) > div > div.FormInput_inputWrap__mjtRx > input[type=text]').send_keys('0339558998')
+#step 5 bam vao dang nhap
+driver.find_element(By.CSS_SELECTOR, '#root > div > div.Login_container__l\+22X > div.Login_content__Clzqv > div.Login_body__t6Xgp > div > button > div').click()
+
+#sau khi thực hiện xong thì chỉ tắt trình duyệt
+# driver.close()
